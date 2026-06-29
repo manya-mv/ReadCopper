47b8c59c6a6cccaccc7a0993672649187a925121
Manya <manya13mehta@gmail.com>
2026-06-29 21:08:36 +0530

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Settings from "./pages/settings";
+import Stats from "./pages/stats";
+import Highlights from "./pages/highlights";
+import Shelves from "./pages/shelves";
+import Notes from "./pages/notes";
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/library" element={<Library />} />
-        <Route path="/stats" element={<Stats />} /> */}
+        <Route path="/stats" element={<Stats />} />
         <Route path="/settings" element={<Settings />} />
+        <Route path="/highlights" element={<Highlights />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/shelves" element={<Shelves />} />
+        {/* <Route path="/library" element={<Library />} />
+        <Route path="/library/currently-reading" element={<CurrentlyReading />} />
+        <Route path="/library/want-to-read" element={<WantToRead />} />
+        <Route path="/library/finished" element={<Finished />} />
+        <Route path="/library/favorites" element={<Favorites />} /> */}
       </Routes>
     </Layout>
   );
---
+++ src/pages/shelves.jsx
@@ -0,0 +1,5 @@
+function Shelves() {
+  return <h1>Shelves</h1>;
+}
+
+export default Shelves;
